fa91f14adc88092e0e397a5263e519d05428237e
Wesley Burch <wesley@hellohydrant.com>
2019-07-26 14:23:34 -0700

    Heroku ENV variables
    adding env variables for s3 storage in heroku

---
+++ config/initializers/shrine.rb
@@ -0,0 +1,32 @@
+require "shrine"
+require "shrine/storage/file_system"
+require "shrine/storage/s3"
+
+s3_options = {
+  bucket: "app-instablam"
+  access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: "us-west-1"
+}
+
+Shrine.storages = {
+  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+  store: Shrine::Storage::S3.new(**s3_options)
+}
+
+Shrine.plugin :activerecord # or :activerecord
+Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
+Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file
+
+# FOR DIRECT UPLOADS
+Shrine.plugin :presign_endpoint, presign_options: -> (request) {
+  # Uppy will send the "filename" and "type" query parameters
+  filename = request.params["filename"]
+  type     = request.params["type"]
+
+  {
+    content_disposition:    "inline; filename=\"#{filename}\"", # set download filename
+    content_type:           type,                               # set content type (defaults to "application/octet-stream")
+    content_length_range:   0..(10*1024*1024),                  # limit upload size to 10 MB
+  }
+}
